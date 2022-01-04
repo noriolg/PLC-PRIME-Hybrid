8bd542b7aa61fa4c675a9cd25b3ecd2e527b9784
nico <noriolg@gmail.com>
2022-01-04 18:28:47 +0100

    First dummy statistics recorded. Batch python code tweaked

--- omnet/simulations/batch.py
+++ omnet/simulations/batch.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import os
 
-nedLibraries = '-n ../src -n ./ -n ../../../inet4.3/src -n ../../../inet4.3/examples -n ../../../inet4.3/tutorials -n ../../../inet4.3/showcases'
+nedLibraries = '-n ../src -n ./ -n ../../../inet4.3/src -n ../../../inet4.3/examples -n ../../../inet4.3/tutorials -n ../../../inet4.3/showcases -n ../../../simPRIME2/src'
 
 
-os.system('../out/gcc-release/src/RESPONSE_Dijon Dijon.ini '+ nedLibraries +' -u Cmdenv -c StarTopology --sim-time-limit=28800s')
-os.system('valgrind --tool=memcheck --log-file=valgrindlog.txt ../out/gcc-release/src/RESPONSE_Dijon finalTest.ini -n ../src -n ./ -n ../../../inet4.3/src -n ../../../inet4.3/examples -n ../../../inet4.3/tutorials -n ../../../inet4.3/showcases -u Cmdenv -c num27 --sim-time-limit=28800s')
+os.system('../out/gcc-release/src/HybridMeter omnetpp.ini '+ nedLibraries +' -u Cmdenv -c NicosConfigurationTest --sim-time-limit=40s')
+#os.system('valgrind --tool=memcheck --log-file=valgrindlog.txt ../out/gcc-release/src/HybridMeter finalTest.ini -n ../src -n ./ -n ../../../inet4.3/src -n ../../../inet4.3/examples -n ../../../inet4.3/tutorials -n ../../../inet4.3/showcases -u Cmdenv -c num27 --sim-time-limit=28800s')
 
 os.chdir('./results')
 os.system('opp_scavetool x *sca -o scalars.csv')
