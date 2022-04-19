55e6be0ab283d3a2e5679fc220add5937be3c724
nico <noriolg@gmail.com>
2022-04-19 21:31:49 +0200

    ALV revised

--- omnet/simulations/vectorACSV.py
+++ omnet/simulations/vectorACSV.py
@@ -10,7 +10,7 @@ import os
 nedLibraries = "-n .:../src:../../../../inet4.3/src:../../../../inet4.3/examples:../../../../inet4.3/tutorials:../../../../inet4.3/showcases:../../../../simprime2/omnet/simPRIME2/simulations:../../../../simprime2/omnet/simPRIME2/src -x inet.emulation;inet.showcases.visualizer.osg;inet.showcases.emulation;inet.clock.common;inet.clock.model;inet.visualizer.osg;inet.examples.voipstream;inet.clock.oscillator;inet.examples.emulation;inet.transportlayer.tcp_lwip;inet.applications.voipstream;inet.clock.base;inet.examples.clock;inet.transportlayer.tcp_nsc --image-path=../../../../inet4.3/images -l ../../../../inet4.3/src/INET -l ../../../../simprime2/omnet/simPRIME2/src/simPRIME2 --debug-on-errors=true "
 
 
-os.system('../src/HybridMeter_dbg HybridMeter.ini  -u Cmdenv '+ nedLibraries +' -c General --sim-time-limit=1500s')
+os.system('../src/HybridMeter_dbg HybridMeter.ini  -u Cmdenv '+ nedLibraries +' -c General --sim-time-limit=800')
 
 
 #path_scalars = sys.argv[1] + ".sca" # Este no lo solemos coger
